c716251623afb8e0d058e19cf5fbc8ecb4b65ba0
mngroon <m.haroon3287zxcv@gmail.com>
2020-09-21 23:00:57 +0900

    Add files via upload
    easy network programming

--- client.py
+++ client.py
@@ -1,34 +1,12 @@
-import numpy as np
-import pygame
-from pygame.locals import * 
-from pygase import Client
-import sys
-from keyhandle import key_handle
+import os
+import socket
 
+server_ip = "127.0.0.1"
+port = 5000
 if __name__ == "__main__":
-    title = "TETRIS"
-    pygame.init()
-    SCREEN = Rect(0, 0, 400, 400)
-    screen = pygame.display.set_mode(SCREEN.size)
-    pygame.display.set_caption(title)
-    
-    client = Client()
-    client.connect_in_thread(port=8080)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_ip,port))
+        s.sendall(b"hello")
+        data = s.recv(1024)
 
-    clock = pygame.time.Clock()
-    while (1):
-        clock.tick(30)
-        # イベント処理
-        for event in pygame.event.get():
-            if event.type == QUIT:  # 閉じるボタンが押されたら終了
-                client.disconnect(shutdown_server=True)
-                pygame.quit()       # Pygameの終了(画面閉じられる)
-                sys.exit()
-            elif event.type == KEYDOWN:
-                key_handle(event.key)
-        with client.access_game_state() as game_state:
-
-            screen.fill((255,255,255))
-            game_state.b_group.draw(screen)
-            game_state.Stage.draw(screen)
-        pygame.display.update()
+        print(repr(data))
--- server.py
+++ server.py
@@ -1,50 +1,20 @@
-import math
-from pygase import Gamestate, Backend
-from Block import Block
-from pygame.locals import * 
-import pygame
+import os
+import socket
 
-Stage = pygame.sprite.RenderUpdates()
-block = Block("test.png",150,0,Stage)
-b_group = pygame.sprite.RenderUpdates()
-b_group.add(block)
-b_group.update()
-initial_game_state = GameState(Stage=Stage,b_group=b_group ,block = block)
 
-def time_step(game_state,dt):
-    ## update gamestatus by time
-    pass
-
-def key_space(game_state):
-    game_state.block.move_dfast()
-    print("space")
-
-def key_down(game_state):
-    game_state.block.move_down()
-    print("down")
-
-def key_left(game_state):
-    game_state.block.move_left()
-    print("left")
-
-def key_right(game_state):
-    game_state.block.move_right()
-    print("right")
-
-def key_quit(game_state):
-    print("quit")
-    pass
-
-backend = Backend(
-        initial_game_state,
-        time_step,
-        event_handlers={"K_SPACE":key_space,
-                        "K_DOWN":key_down,
-                        "K_RIGHT":key_right,
-                        "K_LEFT":key_left,
-                        "K_QUIT":key_quit
-                        }
-       )
-
+ip_address = "127.0.0.1"
+port = 5000
 if __name__ == "__main__":
-    backend.run('localhost', 8080)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ip_address,port))
+        s.listen(1) ## connection
+        while True:
+            conn,addr = s.accept()
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(data)
+
+                    conn.sendall(b"Received: " + data)
