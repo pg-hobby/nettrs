2163155ad761fbb3ea6760445ef4a96718f6136d
LeavaTail <starbow.duster@gmail.com>
2020-09-27 22:52:02 +0900

    clear line

--- server.py
+++ server.py
@@ -10,6 +10,12 @@ port = 5000
 sstage = np.zeros(64).astype("int").reshape([8, 8])
 dstage = np.zeros(64).astype("int").reshape([8, 8])
 
+def Check_Stage(Sstage):
+    is_full = False
+    if np.all(Sstage[7] == 1):
+        is_full = True
+    return is_full
+
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((ip_address,port))
@@ -28,5 +34,8 @@ if __name__ == "__main__":
                     print(sstage + dstage)
                     if blk.isdead == True:
                         del blk
+                        if Check_Stage(sstage):
+                            sstage = np.roll(sstage, 1, axis=0)
+                            sstage[0] = np.zeros(8)
                         blk = Block(sstage)
                     conn.sendall(pickle.dumps([sstage, dstage]))
