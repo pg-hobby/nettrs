5a58eb4ef4f7fa43a5f50ce1b25968b656f8c91f
LeavaTail <starbow.duster@gmail.com>
2020-10-11 23:49:36 +0900

    display opponent game

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ port = 5000
 
 # Block colors     [empty],         [dynamic],     [static]
 colors = np.array([[255, 255, 255], [0, 255, 255], [255, 0, 0]])
+ostage = np.zeros(64).astype("int").reshape([8,8])
 stage = np.zeros(64).astype("int").reshape([8,8])
 sstage = np.zeros(64).astype("int").reshape([8,8])
 dstage = np.zeros(64).astype("int").reshape([8,8])
@@ -19,15 +20,18 @@ dstage = np.zeros(64).astype("int").reshape([8,8])
 if __name__ == "__main__":
     title = "TETRIS"
     pygame.init()
-    SCREEN = Rect(0, 0, 400, 400)
+    SCREEN = Rect(0, 0, 800, 400)
     screen = pygame.display.set_mode(SCREEN.size)
     pygame.display.set_caption(title)
     surface = pygame.surfarray.make_surface(colors[stage.T])
     surface = pygame.transform.scale(surface, (400, 400))
+    surface2 = pygame.surfarray.make_surface(colors[ostage.T])
+    surface2 = pygame.transform.scale(surface2, (400, 400))
 
     clock = pygame.time.Clock()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server_ip, port))
+        player = pickle.loads(s.recv(256))
         while (1):
             clock.tick(30)
             s.sendall(pickle.dumps(-1))
@@ -35,9 +39,14 @@ if __name__ == "__main__":
             if not raw:
                 break
             sstage,dstage = pickle.loads(raw)
-            stage = dstage + np.multiply(sstage, 2)
+            sstage = np.multiply(sstage, 2)
+            ostage = sstage[:, 8 * ((player + 1) % 2): (8 * ((player + 1) % 2)) + 8]
+            # print(sstage[:, 8 * ((player + 1) % 2): (8 * ((player + 1) % 2)) + 8])
+            stage = dstage + sstage[:, 8 * player: (8 * player) + 8]
             surface = pygame.surfarray.make_surface(colors[stage.T])
             surface = pygame.transform.scale(surface, (400, 400))
+            surface2 = pygame.surfarray.make_surface(colors[ostage.T])
+            surface2 = pygame.transform.scale(surface2, (400, 400))
 
             # イベント処理
             for event in pygame.event.get():
@@ -50,12 +59,18 @@ if __name__ == "__main__":
                     if not raw:
                         break
                     sstage,dstage = pickle.loads(raw)
-                    stage = dstage + np.multiply(sstage, 2)
-                    print(stage)
+                    sstage = np.multiply(sstage, 2)
+                    stage = dstage + sstage[:, 8 * player: (8 * player) + 8]
+                    ostage = sstage[:, 8 * ((player + 1) % 2): (8 * ((player + 1) % 2)) + 8]
+                    # print(sstage[:, 8 * ((player + 1) % 2): (8 * ((player + 1) % 2)) + 8])
                     surface = pygame.surfarray.make_surface(colors[stage.T])
                     surface = pygame.transform.scale(surface, (400, 400))
+                    surface2 = pygame.surfarray.make_surface(colors[ostage.T])
+                    surface2 = pygame.transform.scale(surface2, (400, 400))
 
             screen.fill((30, 30, 30))
             screen.blit(surface, (0, 0))
+            screen.blit(surface2, (400, 0))
+            pygame.draw.line(screen, (0, 95, 0), (400, 0), (400, 400), 1)
             pygame.display.update()
 
--- server.py
+++ server.py
@@ -9,42 +9,46 @@ from keyhandle import key_handle
 
 ip_address = "127.0.0.1"
 port = 5000
-sstage = np.zeros(64).astype("int").reshape([8, 8])
+sstage = np.zeros(128).astype("int").reshape([8, 16])
 
-def Check_Stage():
-    global sstage
+def Check_Stage(sstagep):
     is_full = False
-    if np.all(sstage[7] == 1):
+    if np.all(sstagep[7] == 1):
         is_full = True
     return is_full
 
-def threaded_client(conn):
+def threaded_client(conn, player):
     global sstage
+    sstagep = np.zeros(64).astype("int").reshape([8, 8])
     dstage = np.zeros(64).astype("int").reshape([8, 8])
     with conn:
-        blk = Block(sstage)
+        conn.sendall(pickle.dumps(player))
+        blk = Block(sstagep)
         while True:
             raw = conn.recv(2024)
             if not raw:
                 break
             data = pickle.loads(raw)
-            sstage,dstage = key_handle(data, blk)
-            print(sstage + dstage)
+            sstagep,dstage = key_handle(data, blk)
+            #print(sstagep + dstage)
             if blk.isdead == True:
                 del blk
-                blk = Block(sstage)
-            if Check_Stage():
-                for i in reversed(range(sstage.shape[0] - 1)):
-                    sstage[i + 1] = sstage[i]
-                sstage[0] = np.zeros(8)
+                blk = Block(sstagep)
+            if Check_Stage(sstagep):
+                for i in reversed(range(sstagep.shape[0] - 1)):
+                    sstagep[i + 1] = sstagep[i]
+                sstagep[0] = np.zeros(8)
+            sstage[:, 8 * player: (8 * player) + 8] = sstagep
+            print(sstage)
             conn.sendall(pickle.dumps([sstage, dstage]))
 
-
 if __name__ == "__main__":
+    player = 0
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((ip_address,port))
         s.listen(2) ## connection
         while True:
             conn,addr = s.accept()
             print('Connected to: ' + addr[0] + ':' + str(addr[1]))
-            start_new_thread(threaded_client, (conn, ))
+            start_new_thread(threaded_client, (conn, player))
+            player += 1
